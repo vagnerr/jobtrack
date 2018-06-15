f49aea6f47abced021d62d22e9d774cbfd1c6faf
Peter Wise <peter@bloodaxe.com>
2018-06-15 12:18:06 +0000

    make datepick document.open url relative for easlier proxypassing #11

--- stable/website/docroot/smarty_datepick/javascripts/datepick.js
+++ stable/website/docroot/smarty_datepick/javascripts/datepick.js
@@ -7,12 +7,12 @@ function datepick(theme,field) {
 	argMonth = '';
 	argDay = '';
 	argDate = field;
-	open('/smarty_datepick/index.php?type=textfield&theme='+theme+'&date='+argDate.value,'datepick','width=200,height=200');
+	open('smarty_datepick/index.php?type=textfield&theme='+theme+'&date='+argDate.value,'datepick','width=200,height=200');
 }
 function datepick3(theme,field1,field2,field3) {
 	argYear = field1;
 	argMonth = field2;
 	argDay = field3;
 	argDate = '';
-	open('/smarty_datepick/index.php?type=dropdown&theme='+theme+'&date='+argYear.options[argYear.selectedIndex].value+'-'+argMonth.options[argMonth.selectedIndex].value+'-'+argDay.options[argDay.selectedIndex].value,'datepick','width=200,height=200');
+	open('smarty_datepick/index.php?type=dropdown&theme='+theme+'&date='+argYear.options[argYear.selectedIndex].value+'-'+argMonth.options[argMonth.selectedIndex].value+'-'+argDay.options[argDay.selectedIndex].value,'datepick','width=200,height=200');
 }
